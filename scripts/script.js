a83291603a37c3d854018502d6e12a2aab01cad9
Kim Chaewon <tsukigrayskull223@gmail.com>
2025-02-04 23:03:04 +0800

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1029,20 +1029,3 @@
 		
 		renderChannels();
         });
-        
-        document.addEventListener('contextmenu', e => e.preventDefault());
-
-        function ctrlShiftKey(e, keyCode) {
-            return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
-        }
-
-        document.onkeydown = (e) => {
-            if (
-                e.keyCode === 123 ||
-                ctrlShiftKey(e, 'I') ||
-                ctrlShiftKey(e, 'J') ||
-                ctrlShiftKey(e, 'C') ||
-                (e.ctrlKey && e.keyCode === 'U'.charCodeAt(0))
-            )
-                return false;
-        };
